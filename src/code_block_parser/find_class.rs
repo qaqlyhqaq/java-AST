08bc4f077b35652b62e138a143b2c879c268a8d8
lyh <liaoyinghai@yymt.com>
2024-12-19 13:09:07 +0800

    添加 FnMut 实现

--- src/code_block_parser/find_class.rs
+++ src/code_block_parser/find_class.rs
@@ -1,15 +1,28 @@
-use std::marker::Tuple;
-
 struct Closure {
     s : String,
     t : String,
 }
 
-impl<T> FnOnce<(T,)> for Closure {
+impl FnOnce<()> for Closure
+{
+
     type Output = String;
 
-    extern "rust-call" fn call_once(self, _: (T,)) -> Self::Output {
+    extern "rust-call" fn call_once(self, args: ()) -> Self::Output {
         let x = self.s + &*self.t;
         x
+    }
+}
+
+
+#[cfg(test)]
+mod tests{
+    use crate::code_block_parser::find_class::Closure;
+
+    #[test]
+    fn it_works() {
+        let closure = Closure { s: "hello".to_string(), t: "world".to_string() };
+        let string = closure();
+        assert_eq!(string, "helloworld".to_string());
     }
 }
