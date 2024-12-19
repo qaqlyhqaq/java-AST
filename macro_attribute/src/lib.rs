da9fc0a7640aec60226d5998648430391b4ee6cd
lyh <liaoyinghai@yymt.com>
2024-12-19 17:34:03 +0800

    add rust macro .

--- macro_attribute/src/lib.rs
+++ macro_attribute/src/lib.rs
@@ -1,9 +1,18 @@
+#![feature(proc_macro_quote)]
 extern crate proc_macro;
 
-use proc_macro::{ TokenStream};
+use proc_macro::{quote, TokenStream};
+use syn::{parse_macro_input, DeriveInput};
 
 
 #[proc_macro_derive(MyDerive)]
 pub fn my_derive_macro(input: TokenStream) -> TokenStream {
-    input
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let generate = quote!{
+        impl MyDerive for #input {
+
+        }
+    };
+    generate.into()
 }
