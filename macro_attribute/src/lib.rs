7839f25a904f5bc82f3dec57b9a2d14152dbdd19
lyh <liaoyinghai@yymt.com>
2024-12-19 15:55:16 +0800

    add rust macro .

--- macro_attribute/src/lib.rs
+++ macro_attribute/src/lib.rs
@@ -1,4 +1,3 @@
-
 extern crate proc_macro;
 
 use proc_macro::{ TokenStream};
@@ -10,4 +9,11 @@ pub fn my_derive_macro(input: TokenStream) -> TokenStream {
     let mut string = input.to_string();
     string.push_str("//this is commentary !");
     string.parse().unwrap()
+}
+
+
+#[proc_macro_attribute]
+pub fn my_macro(attr: TokenStream, item: TokenStream) -> TokenStream {
+    // 在这里处理attr和item...
+    item
 }
--- macro_attribute/src/macro_proc.rs
+++
@@ -1,5 +0,0 @@
-#[proc_macro_attribute]
-pub fn my_macro(attr: TokenStream, item: TokenStream) -> TokenStream {
-    // 在这里处理attr和item...
-    item
-}
